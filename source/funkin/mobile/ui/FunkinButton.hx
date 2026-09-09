5fb95b36cb182a312e7914949a6cbfdc8a19f1bf
Gx-Droid <carloseduardoyt500@gmail.com>
2026-09-08 23:53:52 -0300

    Fix FunkinButton destroy
    Clear deadZones array in destroy method instead of using FlxDestroyUtil.

--- source/funkin/mobile/ui/FunkinButton.hx
+++ source/funkin/mobile/ui/FunkinButton.hx
@@ -150,7 +150,7 @@ class FunkinButton extends FunkinSprite implements IFlxInput
    */
   override public function destroy():Void
   {
-    deadZones = FlxDestroyUtil.destroyArray(deadZones);
+    deadZones = [];
     currentInput = null;
     input = null;
 
